444ae8f6864f7eccdc41d96b9a7ca3f364ff7cff
Michael Shaw <spencershawiscool@gmail.com>
2023-03-11 18:37:25 -0500

    Pull changes from nes branch

--- include/cpu.h
+++ include/cpu.h
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright 2021 Michael Shaw
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef CPU_H_
 #define CPU_H_
 
@@ -42,13 +66,18 @@ struct cpu
     u8  cycles;
     u8  addrmode;
 
-    void *fw;
-    void *callback;
-};
-typedef u8 *(*cpucallback)(struct cpu *cpu, u16);
+    u8 pmaset;
+    
+    u8 rdonly;
 
-u8 *
-cpu_default_callback(struct cpu *cpu, u16 addr);
+    void *fw;
+    void *cpu_write;
+    void *cpu_read;
+
+    int echooff;
+};
+typedef u8 *(*f_cpu_read)(struct cpu *cpu, u16 addr);
+typedef void *(*f_cpu_write)(struct cpu *cpu, u16 addr, u8 val);
 
 void
 cpu_push(struct cpu *cpu, u8 val);
